3d9cbfb5fdd864db195efca2057d1c923e4bc295
zjzsliyang <zjzsliyang@gmail.com>
2016-10-25 13:09:34 +0800

    init

---
+++ Mathilda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mathilda
+//
+//  Created by Yang on 25/10/2016.
+//  Copyright © 2016 Mathilda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
