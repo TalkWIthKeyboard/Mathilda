c75165c65e40ce31669ff8f57cb43edf4e797d95
Yang Li <zjzsliyang@gmail.com>
2016-11-25 23:52:41 +0800

    1. 初步实现了事件增添界面

--- Mathilda/AddEventViewController.swift
+++ Mathilda/AddEventViewController.swift
@@ -8,22 +8,43 @@
 
 import UIKit
 
-class AddEventViewController: UIViewController, UITableViewDelegate {
-    @IBOutlet var tableView: UITableView!
+class AddEventViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var eventView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        }
+        let eventInfo = UITextField(frame: (cell?.bounds)!)
+        eventInfo.placeholder = "   " + "Title"
+        eventInfo.adjustsFontSizeToFitWidth = true
+        eventInfo.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+        cell?.addSubview(eventInfo)
+        return cell!
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
     
     @IBAction func cancel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-
     @IBAction func done(_ sender: UIBarButtonItem) {
         
         self.dismiss(animated: true, completion: nil)
